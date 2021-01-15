5fb20d58492552f510effd54e7eeb56c7ed748b4
dan <dannyho1238D@gmail,com>
2021-01-14 18:39:50 -0800

    first commit

---
+++ server/routes/users.js
@@ -0,0 +1,32 @@
+const { response } = require('express');
+var express = require('express');
+var router = express.Router();
+const faker = require('faker');
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  const address = faker.address;
+  res.send(
+    {"name":"bob"}
+  )
+});
+
+router.get('/address', function(req, res, next) {
+  const address = faker.address;
+  res.send(
+    {
+      "name":"bob",
+      "data":
+        {
+          country: address.country(),
+          city: address.city(),
+          state: address.state(),
+          zipCode: address.zipCode(),
+          latitude: address.latitude(),
+          longitude: address.longitude()
+        }
+      }
+  )
+});
+
+module.exports = router;
