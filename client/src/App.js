f2c16a8af8a12d9d692826d526d22c0065aa018c
dan <dannyho1238D@gmail,com>
2021-01-14 19:10:52 -0800

    Part 1: Fetching error

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,27 @@
 import './App.css';
+import React, {useState, useEffect} from 'react';
 
 function App() {
+  //State Elements
+  const [name, setName] = useState([])
+
+  useEffect(() => {
+    const getData = () => {
+      fetch("http://10.0.2.15:5000/users")
+      .then(response=>response.json())
+      .then(data => {
+        console.log(data);
+        setName(data)
+      })
+      .catch(err=>console.log(err))
+    }
+    getData();
+  }, [])
+
+
   return (
     <div className="App">
-
+      <h1>Hi my name is {name['name']}</h1>
     </div>
   );
 }
