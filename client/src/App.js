cd3d33b8bec9980257b816415b3533ab2dce3ef5
jae <39254058+sunjaeyoon@users.noreply.github.com>
2021-01-15 21:44:21 -0800

    Adding Images!!!

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,27 @@ import './App.css';
 import React, {useState, useEffect} from 'react';
 
 import Button from '@material-ui/core/Button';
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
+
 import Header from './components/Header';
+import NestedGrid from './components/NestedGrid';
+
+const customTheme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#757ce8',
+      main: '#3f50b5',
+      dark: '#002884',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#ff7961',
+      main: '#f44336',
+      dark: '#ba000d',
+      contrastText: '#000',
+    },
+  },
+});
 
 function App() {
   //State Elements
@@ -43,21 +63,27 @@ function App() {
 
   return (
     <div className="app">
+      
       {/**-------------------Navbar -------------------*/}
-      <Header firstname={name['firstname']} lastname={name['lastname']}/>
+      <MuiThemeProvider theme={customTheme}>
+        <Header firstname={name['firstname']} lastname={name['lastname']}/>
+      </MuiThemeProvider>
       {/**--------------Body-------------------------- */}
       <div className='app__body'>
-      <h1>Hi, My name is {name["firstname"]} {name["lastname"]}</h1>
-      <p>I live in {address['city']} {address['state']} {address['country']}</p>
-      
+        <h1>Hi, My name is {name["firstname"]} {name["lastname"]}</h1>
+        <p>I live in {address['city']} {address['state']} {address['country']}</p>
+        
         <Button onClick={getName} variant="outlined" color="primary">
           Log In
         </Button>
-    
-        <Button onClick={loadImage} variant="outlined" color="primary">
-          Load Images
-        </Button>
+
+        <br></br>
+        
+        <div className="app__grid">
+          <NestedGrid></NestedGrid>
+        </div>
       </div>
+      
     </div>
   );
 }
---
+++ client/src/components/NestedGrid.js
@@ -0,0 +1,72 @@
+import React, { useEffect } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import GridListTileBar from '@material-ui/core/GridListTileBar';
+import ListSubheader from '@material-ui/core/ListSubheader';
+import IconButton from '@material-ui/core/IconButton';
+import InfoIcon from '@material-ui/icons/Info';
+//import tileData from './tileData';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    overflow: 'hidden',
+    backgroundColor: theme.palette.background.paper,
+  },
+  gridList: {
+    width: 500,
+    height: 450,
+  },
+  icon: {
+    color: 'rgba(255, 255, 255, 0.54)',
+  },
+}));
+
+/**
+ * The example data is structured as follows:
+ *
+ * import image from 'path/to/image.jpg';
+ * [etc...]
+ *
+ * const tileData = [
+ *   {
+ *     img: image,
+ *     title: 'Image',
+ *     author: 'author',
+ *   },
+ *   {
+ *     [etc...]
+ *   },
+ * ];
+ */
+export default function NestedGrid() {
+  const classes = useStyles();
+  const tileData = [];
+  
+  useEffect(() => {
+    fetch("http://localhost:5000/users")
+    .then(response=>response.json())
+    .then(data => {
+      console.log(data)
+    })
+    .catch(err=>console.log(err))
+  }, [])
+  
+  return (
+    <div className={classes.root}>
+      <GridList cellHeight={180} className={classes.gridList}>
+        <GridListTile key="Subheader" cols={2} style={{ height: 'auto' }}>
+          <ListSubheader component="div">Images</ListSubheader>
+        </GridListTile>
+        {tileData.map((tile) => (
+          <GridListTile key={tile.img}>
+            <img src={tile.img} alt={tile.title} />
+          </GridListTile>
+        ))}
+      </GridList>
+    </div>
+  );
+}
